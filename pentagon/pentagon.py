58bbdd20d0dc8e276abb33763d8b9c3a2301679d
BlueOakOffical <74090680+BlueOakOffical@users.noreply.github.com>
2020-11-07 12:00:49 +0300

    Add files via upload

---
+++ pentagon/pentagon.py
@@ -0,0 +1,21 @@
+import random
+rand_num = random.randint(0,50)
+
+num=-32
+alerts = 0
+
+while (num != rand_num):
+    num=int(input("Взломай пинтагон\n"))
+    if num == rand_num:
+        print("пинтагон взломан тобою")
+        print("тебя спалили {} hfp".format(alerts))
+        input("print EXIT\n")
+    else:
+        alerts += 1
+        print("тi визвал минтов :(")
+if alerts == 49():
+    print ("тебя посадили на б******")
+    print("тебя спалили {} раз".format(alerts))
+    input("print EXIT\n")
+if num == cheat():
+    print(rand_num)
