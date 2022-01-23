10fb8e61c2759e6c3c80de84741590bda5d9e028
BlueOakOffical <74090680+BlueOakOffical@users.noreply.github.com>
2022-01-23 19:27:29 +0700

    Add files via upload

--- ЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛ/David.py
+++ ЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛ/David.py
@@ -1,9 +1,9 @@
 import shreklib
 shreklib.vyvodPolya(shreklib.vidimost_polya)
-shreklib.bombs()
+shreklib.bombes()
 game=True
 
-while game:
+while game == True:
     stroka = int(input("Введите номер строки"))
     stolb = int(input("Введите номер столбца"))
     if (stolb>1 and stolb<12) or (stroka<1 and stroka>12):
