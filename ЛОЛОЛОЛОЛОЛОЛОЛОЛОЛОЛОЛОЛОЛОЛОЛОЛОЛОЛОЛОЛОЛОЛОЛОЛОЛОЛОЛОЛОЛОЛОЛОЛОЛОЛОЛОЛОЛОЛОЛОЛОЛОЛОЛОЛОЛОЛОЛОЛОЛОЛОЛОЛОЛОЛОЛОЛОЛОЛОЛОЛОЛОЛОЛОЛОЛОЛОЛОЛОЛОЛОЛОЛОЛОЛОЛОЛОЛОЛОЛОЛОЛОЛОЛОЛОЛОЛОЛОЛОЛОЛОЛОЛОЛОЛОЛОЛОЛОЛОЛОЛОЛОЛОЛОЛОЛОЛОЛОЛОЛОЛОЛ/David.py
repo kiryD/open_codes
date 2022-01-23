d79e386df46c76aab5aa5371da9141ec1cc36b88
BlueOakOffical <74090680+BlueOakOffical@users.noreply.github.com>
2022-01-23 18:18:04 +0700

    Add files via upload

---
+++ ЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОЛ/David.py
@@ -0,0 +1,17 @@
+import shreklib
+shreklib.vyvodPolya(shreklib.vidimost_polya)
+shreklib.bombs()
+game=True
+
+while game:
+    stroka = int(input("Введите номер строки"))
+    stolb = int(input("Введите номер столбца"))
+    if (stolb>1 and stolb<12) or (stroka<1 and stroka>12):
+        # передадим не номера строки и столбца, а индексы
+        shreklib.check(stroka-1,stolb-1)
+        shreklib.vyvodPolya(shreklib.vidimost_polya)
+        if shreklib.isOpen():
+            game = False
+    else:
+        game=False
+        
