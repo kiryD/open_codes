f0a503ca1a5539e500b59a81e1576175e080cf8b
BlueOakOffical <74090680+BlueOakOffical@users.noreply.github.com>
2022-03-27 19:27:10 +0700

    Add files via upload

--- Country Simulator test/Country Simulator Test.py
+++ Country Simulator test/Country Simulator Test.py
@@ -1,3 +1,6 @@
+import random
+HOC=15
+b=10
 maxpopularity=100
 start=0
 while True:
@@ -5,25 +8,52 @@ while True:
     if menu=='Start':
         #f=open('save.txt','r')
         #stats=f.readlines()
-        stats={'money':5,'popularity':2}
+        stats={'money':5,'population':2}
         start=1
         while start==1:
-            print('Money : ',+stats['money'],'Population : ',+stats['popularity'])
-            action=input('Buy : Food -1$. Buy : Gas -10$. Add territory : -100$. New day +$ -2 Population. Exit.')
+            print('Money : ',+stats['money'],'Population : ',+stats['population'])
+            action=input('Buy : Food -1$. Buy : Gas -10$. Add territory : -100$. Open war menu. New day +$ -2 Population. Exit.')
             if action=='Food'or action == '1' or action == 'buy food':
                 stats['money']=stats['money']-1
-                stats['popularity']=stats['popularity']+1
+                stats['population']=stats['population']+1
             if action=='Gas' or action=='2':
                 stats['money']=stats['money']-10
-                stats['popularity']=stats['popularity']+5
+                stats['population']=stats['population']+5
             if action=='Add new territory' or action=='3':
                 stats['money']=stats['money']-100
-                stats['popularity']=stats['popularity']+4
+                stats['population']=stats['population']+4
                 maxpopularity+=10
+            if action=='Open war menu' or action=='ow':
+                war_open=1
+                while war_open==1:
+                    war_action=input('Start war. Buy amunition. Exit.')
+                    if war_action=='1':
+                        war_started=input('Attack : 1 of 10 Win. Block : -1 HOC. Leave : -100 Population.')
+                        if war_started=='1':
+                            win_chance=random.randint(0,b)
+                            if win_chance==10:
+                                stats['population']+=100
+                                war_open=0
+                            else:
+                                HOC-=10
+                        if HOC==0 or HOC>=0:
+                            break
+                            exit()
+                        if war_action=='2':
+                            HOC-=1
+                        if war_started=='3':
+                            stats['population']-=100
+                            break
+                    if war_action=='2':
+                        stats['money']-=1000
+                        if b!=1:
+                            b-=1
+                    if war_action=='3':
+                            break
             if action=='New day' or action=='0':
-                stats['money']=stats['money']+stats['popularity']
-                stats['popularity']=stats['popularity']-2
-            if stats['popularity']==0 or stats['popularity']<=0:
+                stats['money']=stats['money']+stats['population']
+                stats['population']=stats['population']-2
+            if stats['population']==0 or stats['population']<=0:
                 input('Game over. Press ENTER')
                 start=0
                 exit()
@@ -31,12 +61,12 @@ while True:
                 input('Game over. Press ENTER')
                 start=0
                 exit()
-            if stats['popularity']>=maxpopularity:
-                satst=stats['popularity']-maxpopularity
-                stats['popularity']-=satst
+            if stats['population']>=maxpopularity:
+                satst=stats['population']-maxpopularity
+                stats['population']-=satst
                 print('Max population!')
             if action=='Exit':
-                f=open('save.lolsaver','w')
-                f.write(stats)
-                f.close
+                break
                 exit()
+    if menu=='Extra' or menu=='2':
+        print('Press f3 to win')
